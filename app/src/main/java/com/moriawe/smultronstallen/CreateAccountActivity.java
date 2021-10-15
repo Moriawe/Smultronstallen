207e1a21061eaa35b8da61a4547215254d6536ac
Moriawe <moriawe22@gmail.com>
2021-10-15 12:06:19 +0200

    Login is working
    Login is now working as it should

---
+++ app/src/main/java/com/moriawe/smultronstallen/CreateAccountActivity.java
@@ -0,0 +1,143 @@
+package com.moriawe.smultronstallen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText userEmailET;
+    private EditText newPasswordET;
+    private EditText confirmPasswordET;
+    private Button createAccButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        userEmailET =findViewById(R.id.userEmailET);
+        newPasswordET = findViewById(R.id.newPasswordET);
+        confirmPasswordET = findViewById(R.id.confirmPasswordET);
+        createAccButton = findViewById(R.id.createAccBtn);
+
+    }
+
+    public void createAccount(View view) {
+
+        String TAG = "Error in Create Account";
+
+        if (checkPassword()) {
+
+            String password = newPasswordET.getText().toString();
+            String email = userEmailET.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        if (user != null) {
+
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+            startActivity(intent);
+
+        } else {
+
+            Toast.makeText(this, "No such user", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* SIGN OUT METHOD
+    private void signOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, MainActivity.class));
+
+    } */
+
+    private boolean checkPassword() {
+
+        if (validateForm()) {
+
+            String stringNewPass = newPasswordET.getText().toString();
+            String stringConfirmPass = confirmPasswordET.getText().toString();
+
+            if (stringNewPass.length() > 6) {
+
+                if (stringNewPass.equals(stringConfirmPass)) {
+                    return true;
+                } else {
+                    Toast.makeText(this, "The passwords doesn't match. Try again.", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } else {
+                Toast.makeText(this, "Password needs to be at least 6 long. Try again.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = userEmailET.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            userEmailET.setError("Required.");
+            valid = false;
+        } else {
+            userEmailET.setError(null);
+        }
+
+        String password = newPasswordET.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            newPasswordET.setError("Required.");
+            valid = false;
+        } else {
+            newPasswordET.setError(null);
+        }
+
+        return valid;
+    }
+
+}
--- app/src/main/java/com/moriawe/smultronstallen/LoginActivity.java
+++ app/src/main/java/com/moriawe/smultronstallen/LoginActivity.java
@@ -1,17 +1,34 @@
 package com.moriawe.smultronstallen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
+
+    private FirebaseAuth mAuth;
+
+    private EditText userEmailET;
+    private EditText userPasswordET;
+    private TextView CreateNewAccountTV;
+    private Button loginBtn;
+
+    static String USERNAME = "Kalle"; //TODO Bara för att testskicka något vidare till MapActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +40,114 @@ public class LoginActivity extends AppCompatActivity {
         String messageFromMainActivity = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_KEY_FROM_MAIN_ACTIVITY);
 
         //Capture LoginActivity's(this activity) TextView and set the string from MainActivity into the TextView in LoginActivity(this activity)
-        TextView textView = (TextView) findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView);
         textView.setText(messageFromMainActivity);
 
-        ////Capture login-btn, set a click-listener on it
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login(view);
-            }
-        });
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn = findViewById(R.id.loginBtn);
+        userEmailET = findViewById(R.id.userEmailET);
+        userPasswordET = findViewById(R.id.newPasswordET);
+        CreateNewAccountTV = findViewById(R.id.CreateNewAccountTV);
+
 
     }
 
-    public void login (View view) {
-        Intent goToMapActivityIntent = new Intent(this, MapActivity.class);
-        EditText userName = (EditText) findViewById(R.id.userName);
-        String userNameStringToMapActivity = userName.getText().toString();
-        goToMapActivityIntent.putExtra(USERNAME,userNameStringToMapActivity);
-        startActivity(goToMapActivityIntent);
-    }
+    /* //Checks if the user is already logged in! Should be in main activity [Jennie]
+    @Override
+    public void onStart() {
+    super.onStart();
+    // Check if user is signed in (non-null) and update UI accordingly.
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+    updateUI(currentUser);
+     */
 
     public void signIn(View view) {
 
+        String TAG = "Error in SignIn";
+        String email = userEmailET.getText().toString();
+        String password = userPasswordET.getText().toString();
+
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        /* //TESTVIEW
+        TextView mailTest = (TextView) findViewById(R.id.TVEmailTest);
+        TextView losenTest = (TextView) findViewById(R.id.TVPasswordTest);
+        mailTest.setText(email);
+        losenTest.setText(password); */
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            sendUserToMap(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            sendUserToMap(null);
+                        }
+
+                    }
+                });
+
     }
+
+
+    private void sendUserToMap(FirebaseUser user) {
+
+        if (user != null) {
+
+            Intent goToMapActivityIntent = new Intent(this, MapActivity.class);
+
+            //TODO Skicka med username or nickname
+            /* EditText userName = (EditText) findViewById(R.id.userEmailET);
+            String userNameStringToMapActivity = userName.getText().toString();
+            goToMapActivityIntent.putExtra(USERNAME,userNameStringToMapActivity); */
+
+            startActivity(goToMapActivityIntent);
+
+        } else {
+
+            Toast.makeText(this, "No such user", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void createAccount(View view) {
+        Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = userEmailET.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            userEmailET.setError("Required.");
+            valid = false;
+        } else {
+            userEmailET.setError(null);
+        }
+
+        String password = userPasswordET.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            userPasswordET.setError("Required.");
+            valid = false;
+        } else {
+            userPasswordET.setError(null);
+        }
+
+        return valid;
+    }
+
 }
