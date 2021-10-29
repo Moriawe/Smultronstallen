691ba0230fc92a8654a903b5231d7752ca46f08b
Tobias <tobb-on@hotmail.com>
2021-10-29 05:30:16 +0200

    listupdate

--- app/src/main/java/com/moriawe/smultronstallen/ListFragment.java
+++ app/src/main/java/com/moriawe/smultronstallen/ListFragment.java
@@ -65,21 +65,6 @@ public class ListFragment extends Fragment implements ListAdapter.OnClickListIte
             });//end Provider
         });
 
-
-//        EditText editText = view.findViewById(R.id.edittext);
-//        editText.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//            }
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//            }
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                filter(s.toString());
-//            }
-//        });
-
         return view;
 
     }
